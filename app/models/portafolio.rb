e09526809967ce4621866202af1ac3f6ab3f7e55
ZarakiLanelot <kenpachi2783@gmail.com>
2019-04-29 17:50:20 -0600

    Integrando acción INDEX

--- app/models/portafolio.rb
+++ app/models/portafolio.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: portafolios
+#
+#  id               :bigint           not null, primary key
+#  titulo           :string
+#  subtitulo        :string
+#  cuerpo           :text
+#  imagen_principal :text
+#  imagen_miniatura :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Portafolio < ApplicationRecord
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: skills
+#
+#  id                   :bigint           not null, primary key
+#  titulo               :string
+#  porcentaje_utilizado :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 class Skill < ApplicationRecord
 end
